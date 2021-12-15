554ac43cda2bf710d15e93eb72f541e32dcad389
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-12-15 14:14:08 +0200

    Creating MultipicController and moving methods Multipic() and AddImg() from BrandController into MultipicController

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -154,46 +154,6 @@ class BrandController extends Controller
     }
 
 
-    // Multi images All
-    public function Multipic(){
-        $images = Multipic::orderBy('id', 'desc')->paginate(
-            $perPage = 10, $columns = ['*'], $pageName = 'images'
-         ); // Taking all data (with paginate() OR with get() OR with all()) from the db table and assigning it into $images; ordering it by id in desc  ; getting all data from Multipic model
-
-        return view('admin.multipic.index', compact('images')); // passing $images with compact to the index page
-    }
-
-
-    // Add multi images
-    public function AddImg(Request $request){
-        $validated = $request->validate([
-            'image' => 'required',
-            ]
-        );
-
-        $image = $request->file('image'); // passing the uploaded image into a variable $brand_image
-
-        // Foreach loop for uploading multiple images at once
-        foreach($image as $multi_image){
-            
-            // If you have Intervention Image Library
-            $name_gen = hexdec(uniqid()).'.'.$multi_image->getClientOriginalExtension();
-            Image::make($multi_image)->resize(300, 300)->save('image/multi/'.$name_gen);
-            $last_img = 'image/multi/'.$name_gen;
-            /*********************************/
-
-            // Eloquent ORM
-            Multipic::insert([ // "Brand::" is the name of the model in app/Models/Brand.php
-                'image' => $last_img,         
-                'created_at' => Carbon::now()
-            ]);
-        } 
-
-        return redirect()->back()->with('success', 'Images added successfully'); // redirect to previous page with message displaying for success
-
-    }
-
-
     // Logout
     public function Logout(){
         Auth::logout();
