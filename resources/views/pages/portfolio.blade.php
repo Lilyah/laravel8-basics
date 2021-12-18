92ae9bdfd5d0e16902aca78823ee0ed4ed221463
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-12-18 19:20:31 +0200

    portfolio.blade: Added html Breadcrumbs

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -2,6 +2,22 @@
 
 @section('home_content')
 
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Portolio</h2>
+          <ol>
+            <li><a href="index.html">Home</a></li>
+            <li>Portolio</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+
     <!-- ======= Portfolio Section ======= -->
     <section id="portfolio" class="portfolio">
   <div class="container">
