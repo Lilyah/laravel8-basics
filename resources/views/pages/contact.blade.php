c1e2938ddff0bddf7dbf00590360f77c7a011d7a
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-12-20 13:24:43 +0200

    views/pages/contact.blade: Setup the html for contact form

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -59,36 +59,41 @@
 
         </div>
 
-        @endif
+      @endif
 
         <div class="row mt-5 justify-content-center" data-aos="fade-up">
           <div class="col-lg-10">
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+          <div class="info-wrap">
+            <!-- Displaying success messages after some action in the page -->
+            @if(session('success'))
+              <div class="alert alert-success alert-dismissible fade show" role="alert">
+                  <strong>{{ session('success') }}</strong>
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+            @endif
+
+            <form action="{{ route('contact.form') }}" method="post">
+            @csrf
               <div class="form-row">
                 <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validate"></div>
+                  <input type="text" name="name" class="form-control" placeholder="Your Name"/>
                 </div>
                 <div class="col-md-6 form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validate"></div>
+                  <input type="email" class="form-control" name="email" placeholder="Your Email"/>
                 </div>
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validate"></div>
+                <input type="text" class="form-control" name="subject" placeholder="Subject"/>
               </div>
               <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                <div class="validate"></div>
+                <textarea class="form-control" name="message" rows="5" placeholder="Message"></textarea>
               </div>
-              <div class="mb-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
+
+              <div class="text-center">
+                <button class="btn btn-success" type="submit">Send Message</button>
               </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
             </form>
+      </div>
           </div>
 
         </div>
