f27eb8389f9f5a56dcc19dfaf06dc3011967cca9
Lilqna Vankova <lilyah@Lilqnas-Air.lan>
2021-12-21 15:04:23 +0200

    views/admin/body/change_password.blade: Implementint error messages and method of the form

--- resources/views/admin/body/change_password.blade.php
+++ resources/views/admin/body/change_password.blade.php
@@ -7,20 +7,33 @@
 		<h2>Change Password</h2>
 	</div>
 	<div class="card-body">
-		<form class="form-pill">
+		<form method="post" action="{{ route('password.update') }}" class="form-pill">
+            @csrf
             <div class="form-group">
 				<label for="exampleFormControlPassword3">Current Password</label>
-				<input type="password" class="form-control" id="exampleFormControlPassword3" placeholder="Current Password">
-			</div>
+				<input type="password" class="form-control" name="old_password" id="current_password" placeholder="Current Password">
+                @error('old_password')
+                <span class="text-danger">{{ $message }}</span>
+                <br>
+                @enderror			
+            </div>
 
 			<div class="form-group">
 				<label for="exampleFormControlInput3">New Password</label>
-				<input type="password" class="form-control" id="exampleFormControlInput3" placeholder="Enter New Password">
+				<input type="password" class="form-control" name="password" id="password" placeholder="Enter New Password">
+                @error('password')
+                <span class="text-danger">{{ $message }}</span>
+                <br>
+                @enderror	
 			</div>
 
 			<div class="form-group">
 				<label for="exampleFormControlPassword3">Confirm Password</label>
-				<input type="password" class="form-control" id="exampleFormControlPassword3" placeholder="Confirm Password">
+				<input type="password" class="form-control" name="password_confirmation" id="password_confirmation" placeholder="Confirm Password">
+                @error('password_confirmation')
+                <span class="text-danger">{{ $message }}</span>
+                <br>
+                @enderror	
 			</div>
 
             <button type="submit" class="btn btn-primary btn-defaul">Save</button>
